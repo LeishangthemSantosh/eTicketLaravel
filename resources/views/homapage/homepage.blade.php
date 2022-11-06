62a3a42aa819ae97fabd96960083b68e6eaef017
Leishangthem Santosh <santoshleishangthem1997@gmail.com>
2022-11-06 23:30:38 +0530

    template layout

---
+++ resources/views/homapage/homepage.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('header')
+    @include('layout.partails.header')
+@endsection
+
+@section('body')
+    @include('homapage.partails.banner')
+@endsection
+
+@section('footer')
+    @include('layout.partails.footer')
+@endsection
