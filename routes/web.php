cfc0038cbeff26e6449d46f17b3a5f7600e5d4bd
robatsonmaibam <robatson.dev@inindia.tech.com>
2022-11-17 22:40:43 +0530

    tttt

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homapage.homepage');
 });
+Route::get('/test', function () {
+    return view('layout.admin');
+});
